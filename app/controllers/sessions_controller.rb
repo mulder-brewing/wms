cdcc7d4f63ebb22c5a1724f0ee8cd1fb396a83dc
Not My Real Name <noreply@example.com>
2019-05-27 09:38:22 -0400

    disabled users and companies blocked from logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) && user.enabled && user.company.enabled
       log_in(user)
       redirect_to root_url
     else

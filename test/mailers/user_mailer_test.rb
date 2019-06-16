ce451d8de62929becefb57641f6305c58991a1cb
Ryan <rmulder2007@hotmail.com>
2019-06-16 10:04:25 -0400

    password reset email tests are done

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,17 +4,25 @@ class UserMailerTest < ActionMailer::TestCase
 
   def setup
     @regular_user = users(:regular_user)
+    @regular_user.password = "Testpassword1$$"
+  end
+
+  def check_email(mail, user, subject)
+    assert_equal subject, mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["no-reply@mulderwms.net"], mail.from
+    assert_match user.full_name, mail.body.encoded
+    assert_match user.username, mail.body.encoded
+    assert_match user.password, mail.body.encoded
   end
 
   test "create_user" do
-    @regular_user.password = "Testpassword1$$"
     mail = UserMailer.create_user(@regular_user)
-    assert_equal "Account created for Mulder WMS", mail.subject
-    assert_equal [@regular_user.email], mail.to
-    assert_equal ["no-reply@mulderwms.net"], mail.from
-    assert_match @regular_user.full_name, mail.body.encoded
-    assert_match @regular_user.username, mail.body.encoded
-    assert_match @regular_user.password, mail.body.encoded
+    check_email(mail, @regular_user, "Account created for Mulder WMS")
   end
 
+  test "password_reset" do
+    mail = UserMailer.password_reset(@regular_user)
+    check_email(mail, @regular_user, "Password reset for Mulder WMS")
+  end
 end

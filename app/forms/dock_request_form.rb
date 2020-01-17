d5295c8e5174415ce199c57f37ad389708077902
Ryan <rmulder2007@hotmail.com>
2020-01-17 16:25:54 -0500

    dock queue = not fun :(

---
+++ app/forms/dock_request_form.rb
@@ -0,0 +1,29 @@
+class DockRequestForm < BasicRecordForm
+
+  delegate  :primary_reference, :primary_reference=,
+            :phone_number, :phone_number=,
+            :text_message, :text_message=,
+            :note, :note=,
+            :dock_group_id, :dock_group_id=,
+            to: :@record
+
+  def self.model_name
+    ActiveModel::Name.new(self, nil, "DockRequest")
+  end
+
+  def initialize(*)
+    super
+  end
+
+  def prep_record(params)
+    super(params)
+    unless params.has_key?(:id)
+      @record.dock_group_id = params[:dock_group_id]
+    end
+  end
+
+  def permitted_params
+    [:primary_reference, :phone_number, :text_message, :note, :dock_group_id]
+  end
+
+end
